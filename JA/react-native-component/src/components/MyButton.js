15ef16c30a9bacdb85691e51640a83b5e68160ba
jungahzzzang <jungahzzzang@gmail.com>
2022-07-03 23:50:58 +0900

    챕터 3까지 완료

---
+++ JA/react-native-component/src/components/MyButton.js
@@ -0,0 +1,27 @@
+import React from "react";  //리액트를 불러와 사용하겠다.
+import { TouchableOpacity, Text } from "react-native";
+import PropTypes from 'prop-types';
+
+const MyButton = props => {
+    //console.log(props);
+    return (
+        <TouchableOpacity
+        style={{backgroundColor: '#3498db', padding: 16, margin: 10, borderRadius: 8}}
+        onPress={()=>props.onPress()}
+        >
+            <Text style={{color:'white', fontSize: 24}}>{props.children || props.title}</Text>
+        </TouchableOpacity>
+    );
+};
+
+MyButton.defaultProps = {
+    title: 'Button',
+};
+
+MyButton.propTypes = {
+    title: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired,
+}
+
+
+export default MyButton;
