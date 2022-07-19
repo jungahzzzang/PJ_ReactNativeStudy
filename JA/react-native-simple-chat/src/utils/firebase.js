07623e7190d9f6ae4a7d988583bb8f48ce6ee719
MIRAE-JUNGAH <jungahzzzang@gmail.com>
2022-07-19 17:40:58 +0900

    파이어베이스 버전 수정

--- JA/react-native-simple-chat/src/utils/firebase.js
+++ JA/react-native-simple-chat/src/utils/firebase.js
@@ -1,12 +1,21 @@
-import {initializeApp} from 'firebase/app';
-import {getAuth} from 'firebase/auth';
+// import {initializeApp} from 'firebase/compat/app';
+// import {getAuth} from 'firebase/compat/auth';
+// import config from '../firebase.json';
+
+// export const app = initializeApp(config);
+
+// const Auth = getAuth(app);
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 import config from '../firebase.json';
 
-const app = initializeApp(config);
+export default firebase.initializeApp(config);
 
-const Auth = getAuth(app);
+export const authService = firebase.auth();
 
 export const login = async ({email, password}) => {
     const {user} = await Auth.signInWithEmailAndPassword(email, password);
     return user;
 }
+
